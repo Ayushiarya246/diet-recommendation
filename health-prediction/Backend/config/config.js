a182d36547ad7c0be6dbcc0722c7edc1246b8b20
Ayushi Arya <ayushi010btece22@igdtuw.ac.in>
2025-11-02 17:13:27 +0530

    Fix Postgre config

---
+++ health-prediction/Backend/config/config.js
@@ -0,0 +1,29 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export default {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
+    dialect: "postgres",
+    logging: false
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
+    logging: false
+  }
+};
