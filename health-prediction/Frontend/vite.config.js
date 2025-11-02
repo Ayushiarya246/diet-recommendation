3a07cb82f943102f3390616cf9ef47500361cdc3
Ayushi Arya <ayushi010btece22@igdtuw.ac.in>
2025-11-02 21:47:16 +0530

    modified vite config

--- health-prediction/Frontend/vite.config.js
+++ health-prediction/Frontend/vite.config.js
@@ -1,16 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
-  base: "./", // ✅ Important for deployment
+  plugins: [react()],
+  base: "/",
   server: {
     host: true,
-    port: 5173,
+    port: 5173
   }
 })
-
