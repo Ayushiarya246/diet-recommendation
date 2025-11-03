a6a05782a756f9888f9d2d7e28322086916875a9
Ayushi Arya <ayushi010btece22@igdtuw.ac.in>
2025-11-03 16:21:39 +0530

    modified some pages

--- health-prediction/Frontend/src/context/AuthContext.jsx
+++ health-prediction/Frontend/src/context/AuthContext.jsx
@@ -100,12 +100,13 @@ const submitHealthForm = async (formData, navigateToPrediction) => {
     // ✅ After saving → Send request to Python server
     console.log("➡️ Requesting Prediction...");
     const predictRes = await fetch(`${NODE_API}/api/predict/recommendation`, {
-      method: "GET",
+      method: "POST",
       headers: {
+        "Content-Type": "application/json",
         "Authorization": `Bearer ${token}`,
       },
+      body: JSON.stringify(fullData), // ✅ send data!
     });
-
 
 
 
